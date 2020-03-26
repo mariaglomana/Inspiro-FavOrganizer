a52659a9b774701f2275d670f5a7747e7cd6a36a
Maria Garcia de Lomana <mariagarciadelomana@gmail.com>
2020-03-26 10:45:02 +0100

    push last updates from february

--- src/services/art.js
+++ src/services/art.js
@@ -2,26 +2,29 @@ const MET_SEARCH_ENDPOINT =
   "https://collectionapi.metmuseum.org/public/collection/v1/search?q=";
 const MET_OBJECT_ENDPOINT =
   "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
-let arrIDsResults = [];
-let arrResults = [];
 
-async function getarrIDsResults(searchText) {
-  fetch(MET_SEARCH_ENDPOINT + searchText, {
+function getarrIDsResults(searchText) {
+  return fetch(MET_SEARCH_ENDPOINT + searchText, {
     method: "GET"
-  })
-    .then(response => response.json())
-    .then(data => {
-      arrIDsResults = data.objectIDs;
-      console.log(arrIDsResults);
-    })
-    .catch(function(error) {
-      console.error("Looks like there was a problem: \n", error);
-    });
-
-  return arrIDsResults;
+  });
 }
 
-async function getArtFromAPI(searchText) {
+const mapArtData = data => {
+  const artObject = {};
+  artObject.type = "art";
+  artObject.id = data.objectID;
+  artObject.title = data.title;
+  artObject.authors = data.artistDisplayName;
+  artObject.image = data.primaryImageSmall;
+  artObject.year = data.objectDate;
+  return artObject;
+};
+
+function getArtFromAPI(searchText) {
+  const promise = new Promise();
+
+  const arrayPromises = [];
+
   // fetch(MET_SEARCH_ENDPOINT + searchText, {
   //   method: "GET"
   // })
@@ -29,25 +32,29 @@ async function getArtFromAPI(searchText) {
   //   .then(data => {
   //     arrIDsResults = data.objectIDs;
   //     console.log(arrIDsResults);
-  getarrIDsResults(searchText).then(arrIDsResults => {
-    for (let i = 0; i < 50; i++) {
-      const result = arrIDsResults[i];
-      fetch(MET_OBJECT_ENDPOINT + result)
-        .then(response => response.json())
-        .then(result => {
-          let artObject = {};
-          artObject.type = "art";
-          artObject.id = result.objectID;
-          artObject.title = result.title;
-          artObject.authors = result.artistDisplayName;
-          artObject.image = result.primaryImageSmall;
-          artObject.year = result.objectDate;
-          arrResults.push(artObject);
-          console.log(arrResults);
-        });
-    }
-  });
-  return arrResults;
+  getarrIDsResults(searchText)
+    .then(response => response.json())
+    .then(arrIDsResults => {
+      console.log(arrIDsResults);
+
+      for (let i = 0; i < 50; i++) {
+        const result = arrIDsResults[i];
+        const resPromise = fetch(MET_OBJECT_ENDPOINT + result).then(response =>
+          response.json()
+        );
+        arrayPromises.push(resPromise);
+      }
+
+      Promise.all(arrayPromises).then(detailArtData => {
+        const arrayResult = detailArtData.forEach(mapArtData);
+        promise.resolve(arrayResult);
+      });
+    })
+    .catch(function(error) {
+      console.error("Looks like there was a problem: \n", error);
+    });
+
+  return promise;
 }
 
 export default getArtFromAPI;
